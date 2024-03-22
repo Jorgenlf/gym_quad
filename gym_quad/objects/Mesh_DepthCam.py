e76df2b517d30ec9a6bc614b3fff16acfa768dbe
Jorgen <jorgenlf@stud.ntnu.no>
2024-03-22 23:21:15 +0100

    Prep for mesh depthcam and obstacles

---
+++ gym_quad/objects/Mesh_DepthCam.py
@@ -0,0 +1,4 @@
+#Use package trimesh (and mesh-depth library if trimesh is slow)
+#Pyvista for visualization of the mesh make a polydata object
+#Pyvista oceanic colormaps are nice.
+#Pyvista can create a mesh (might have to run .triangulate() on the mesh object)
