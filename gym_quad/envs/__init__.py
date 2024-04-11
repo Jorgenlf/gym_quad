cd5de280a8552f8453a297356100f16c7f237bea
Jorgen <jflind99@gmail.com>
2024-04-11 14:03:48 +0200

    more cleanup

--- gym_quad/envs/__init__.py
+++ gym_quad/envs/__init__.py
@@ -1,3 +1,2 @@
 #Only needed such that the gym environment can be imported to train3d.py and run3d.py from the gym_quad package
-from gym_quad.envs.waypoint_planner import WaypointPlanner
 from gym_quad.envs.LV_VAE import LV_VAE
