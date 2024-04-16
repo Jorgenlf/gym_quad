7c6bcfbed73960578ee000f0a74951574039fdb9
eirikrb <eirikrb@stud.ntnu.no>
2024-04-16 16:44:28 +0200

    Added render class, distortion fix, new m2d script

--- pytorch3d/mesh_to_depth.py
+++ pytorch3d/mesh_to_depth.py
@@ -18,11 +18,10 @@ from pytorch3d.renderer import (
     TexturesVertex,
 )
 
-
 # Camera globals
 IMG_SIZE = (240, 320)           # (H, W) of physical depth cam images AFTER the preprocessing pipeline
 FOV = 60                        # Field of view in degrees, init to correct value later
-MAX_MEASURABLE_DEPTH = 10.0     # Maximum measurable depth, initialized to k here but is 10 IRL
+MAX_MEASURABLE_DEPTH = 5.0     # Maximum measurable depth, initialized to k here but is 10 IRL
 
 # Get GPU if available (it should)
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -100,16 +99,16 @@ class SphereScene:
 # Test sphere obstacle and scene
 # For the position vectors, +X points left, and +Y points up and +Z points inwards
 pos1 = torch.tensor([0.0, 0.0, 0.0])
-pos2 = torch.tensor([0.0, 5.0, 0.0])
+pos2 = torch.tensor([0.0, 2.0, 0.0])
 #pos3 = torch.tensor([4.0, 0.0, 1.5])
 unit_sphere_path = "./sphere.obj"
-sphere1 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=2.0, center_position=pos1)
-#sphere2 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=2.0, center_position=pos2)
+sphere1 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=1.0, center_position=pos1)
+sphere2 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=1.0, center_position=pos2)
 #sphere3 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=4.0, center_position=pos3)
 
 # Sphere 3 is only partly visible, as it is at the max measurable depth
 
-spheres = [sphere1]#, sphere2]#, sphere3]
+spheres = [sphere1, sphere2]#, sphere3]
 spherescene = SphereScene(device=device, sphere_obstacles=spheres)
 meshes = spherescene.scene
 
@@ -126,24 +125,74 @@ The transformation from world -> view happens after applying a rotation (R) and 
 # In the sim we plan to get the R,T matrices from the drone and use them to update the camera object at evry timestep (must handle transfomations and stuff properly)
 # For now we just initialize the camera to no rotation, looking at the origin from a distance of 10 units in the +Z direction
 R = torch.tensor([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]).unsqueeze(0).to(device)
+print(torch.tensor([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]).shape)
+print(R.shape)
 T = torch.zeros((1, 3)).to(device)
-T[0, 2] = -5.0
-print(f"R: {R}")
-print(f"T: {T}")
+T[0, 0] = 0.0 # Move camera x units in the +X direction (inwards)
+T[0, 1] = 0.0 # Move camera x units in the +Y direction (upwards)
+T[0, 2] = -5.0 # Move camera x units in the +Z direction (in/out)
+#T = -T
+print(f"R:\n {R}")
+print(f"\nT orig:\n {T}")
+
+# The rotation matrix R rotates the whole camera about the world origin, not around the camera frame axes. 
+# The translation matrix T moves the camera in the world coordinate system.
 
 #K = torch.zeros((1, 4, 4)).to(device)
 #K[0, :, :] = torch.tensor([[639.0849609375, 0.0, 644.4653930664062],[0.0, 639.0849609375, 364.2340393066406], [0.0, 0.0, 1.0], [0.0, 1.0, 0.0]])
-camera = FoVPerspectiveCameras(device=device, R=R, T=T, fov=FOV)
+#camera = FoVPerspectiveCameras(device=device, R=R, T=T, fov=FOV)
 
-R1 = look_at_rotation(camera_position=camera.get_camera_center(), at = pos1)[0].unsqueeze(0).to(device)
-print(f"R: {R1}")    
-camera.R = R
+#R1 = look_at_rotation(camera_position=camera.get_camera_center(), at = pos1)[0].unsqueeze(0).to(device)
+#R1 = torch.tensor([[1.0, 0.0, 0.0], [0.0, -1.0, 0.0], [0.0, 0.0, -1.0]]).unsqueeze(0).to(device)
+#print(f"\nRw_to_v from look_at:\n {R1}")    
+#camera.R = R1
+camera = FoVPerspectiveCameras(device=device, R=R, T=-T, fov=FOV)
+# camera1 = FoVPerspectiveCameras(device=device, R=R1, T=T, fov=FOV)
+print(f'Camera position in world frame: {camera.get_camera_center()}')
 
-camera1 = FoVPerspectiveCameras(device=device, R=R1, T=T, fov=FOV)
 
 
-pos = camera1.get_camera_center()
-print(f"Camera position: {pos}")
+
+
+# Render the plotly figure
+#fig = plot_scene({
+#        "subplot1_title": {
+#            "mesh_trace_title": meshes
+#        },
+#    }, viewpoint_cameras=camera) 
+#fig.show()
+
+
+# pos = camera.get_camera_center()
+# print(f"\nCamera position:\n {pos}")
+# camera_orientation = camera.get_world_to_view_transform().inverse().get_matrix()[:, :3].cpu().numpy()
+# print(f"\nCamera orientation:\n {camera_orientation}")
+# print(f'wordl to view:\n {camera.get_world_to_view_transform().get_matrix()}')
+# print(f'full proj transform:\n {camera.get_full_projection_transform().get_matrix()}')
+
+# wtv = camera.get_world_to_view_transform().get_matrix()
+# print(f"\nWorld to view transform:\n {wtv}")
+# vtw = wtv.inverse()
+# print(f"\nView to world transform:\n {vtw}")
+
+# # translate camera in world space
+# T = T + torch.tensor([[1.0, 0.0, 0.0]]).to(device)
+# camera.T = -T
+
+# print(f"\nCamera position after translation:\n {camera.get_camera_center()}")
+
+# pos = camera.get_camera_center()
+# print(f"\nCamera position:\n {pos}")
+# camera_orientation = camera.get_world_to_view_transform().inverse().get_matrix()[:, :3].cpu().numpy()
+# print(f"\nCamera orientation:\n {camera_orientation}")
+# print(f'wordl to view:\n {camera.get_world_to_view_transform().get_matrix()}')
+# print(f'full proj transform:\n {camera.get_full_projection_transform().get_matrix()}')
+
+# wtv = camera.get_world_to_view_transform().get_matrix()
+# print(f"\nWorld to view transform:\n {wtv}")
+# vtw = wtv.inverse()
+# print(f"\nView to world transform:\n {vtw}")
+
 
 # Initialize rasterizer
 # Rasterization maps the 3D mesh to a 2D image for the given camera.
@@ -151,14 +200,14 @@ raster_settings = RasterizationSettings(
     image_size=IMG_SIZE, 
     blur_radius=0.0, 
     faces_per_pixel=1, # Keep at 1, dont change
+    #perspective_correct=True
 )
 rasterizer = MeshRasterizer(
-    cameras=camera1, 
+    cameras=camera, 
     raster_settings=raster_settings
 )
 
 fragments = rasterizer(meshes)
-#print(fragments)
 
 # **zbuf**: FloatTensor of shape (N, image_size, image_size, faces_per_pixel) giving the NDC z-coordinates of the nearest faces at each pixel, sorted in ascending z-order.
 zbuf = fragments.zbuf#.cpu().numpy() # (N, H, W, K), 
@@ -176,26 +225,25 @@ plt.colorbar()
 plt.axis("off")
 plt.savefig("./depth_map.png")
 
-#prnit min and max depth
-print(f"Min depth: {depth.min()}")
+print(f"\nMin depth: {depth.min()}")
 print(f"Max depth: {depth.max()}")
 
 
 # Time how many full depth images can be rendered per second for given device
-import time
-start = time.time()
-for i in range(100):
+# import time
+# start = time.time()
+# for i in range(100):
 
-    # Testing if updating R and T for the camera object is tideous - it is not.
-    R, T = look_at_view_transform(2, 10, 0)
-    camera.T = T.to(device)
-    camera.R = R.to(device)
+#     # Testing if updating R and T for the camera object is tideous - it is not.
+#     R, T = look_at_view_transform(2, 10, 0)
+#     camera.T = T.to(device)
+#     camera.R = R.to(device)
 
-    fragments = rasterizer(meshes)
-    zbuf = fragments.zbuf
-    depth = torch.squeeze(zbuf)
-    depth[depth == -1.0] = MAX_MEASURABLE_DEPTH
+#     fragments = rasterizer(meshes)
+#     zbuf = fragments.zbuf
+#     depth = torch.squeeze(zbuf)
+#     depth[depth == -1.0] = MAX_MEASURABLE_DEPTH
 
-end = time.time()
-print(f"Time for 100 renders with {device}: {end-start} seconds")
-print(f"FPS with {device}: {100/(end-start)}") # FPS: 
+# end = time.time()
+# print(f"Time for 100 renders with {device}: {end-start} seconds")
+# print(f"FPS with {device}: {100/(end-start)}") # FPS: 
---
+++ pytorch3d/mesh_to_depth_2.py
@@ -0,0 +1,259 @@
+import torch
+import matplotlib.pyplot as plt
+from pytorch3d.io import load_objs_as_meshes, load_obj
+from pytorch3d.structures import Meshes, join_meshes_as_batch, join_meshes_as_scene
+from pytorch3d.vis.plotly_vis import AxisArgs, plot_batch_individually, plot_scene
+from pytorch3d.vis.texture_vis import texturesuv_image_matplotlib
+import utils.plot_image_grid as plot_image_grid
+from pytorch3d.renderer.mesh.textures import Textures
+from pytorch3d.renderer import (
+    FoVPerspectiveCameras, look_at_view_transform, look_at_rotation,
+    PointLights,
+    MeshRenderer,
+    MeshRasterizer,
+    RasterizationSettings,
+    SoftSilhouetteShader,
+    SoftPhongShader,
+    Materials,
+    TexturesVertex,
+)
+
+# Camera globals
+IMG_SIZE = (240, 320)           # (H, W) of physical depth cam images AFTER the preprocessing pipeline
+FOV = 60                        # Field of view in degrees, init to correct value later
+MAX_MEASURABLE_DEPTH = 5.0      # Maximum measurable depth, initialized to k here but is 10 IRL
+
+# Get GPU if available (it should)
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+print(f'Using device: {device}')
+
+
+class SphereMeshObstacle:
+    def __init__(self, 
+                 device: torch.device, 
+                 path: str,
+                 radius: float,
+                 center_position: torch.Tensor):
+        self.device = device
+        self.path = path                                    # Assumes path points to UNIT sphere .obj file
+        self.radius = radius
+        self.center_position = center_position.to(device)   # Centre of the sphere in world frame
+        # May include textures later for visualization in non-depth mode
+
+        self.mesh = load_objs_as_meshes([path], device=self.device)
+        self.mesh.scale_verts_(scale=self.radius)
+        self.mesh.offset_verts_(vert_offsets_packed=self.center_position)
+    
+    def resize(self, new_radius: float):
+        self.mesh.scale_verts_(scale=new_radius/self.radius)
+        self.radius = new_radius
+    
+    def move(self, new_center_position: torch.Tensor):
+        new_center_position = new_center_position.to(self.device)
+        self.mesh.offset_verts_(vert_offsets_packed=new_center_position-self.center_position)
+        self.center_position = new_center_position
+
+    def set_device(self, new_device: torch.device):
+        self.device = new_device
+        self.mesh.to(new_device)
+        self.center_position.to(new_device)
+    
+
+class SphereScene:
+    def __init__(self, 
+                 device: torch.device, 
+                 sphere_obstacles: list):
+        self.device = device
+        self.sphere_obstacles = sphere_obstacles # List of SphereMeshObstacle objects
+        self.meshes = [sphere.mesh for sphere in sphere_obstacles]
+        self.joined_scene = join_meshes_as_scene(meshes=self.meshes, include_textures=False) # May inlude textures later
+
+    def resize_sphere(self, sphere_idx: int, new_radius: float):
+        self.sphere_obstacles[sphere_idx].resize(new_radius)
+    
+    def move_sphere(self, sphere_idx: int, new_center_position: torch.Tensor):
+        self.sphere_obstacles[sphere_idx].move(new_center_position)
+    
+    def add_sphere(self, new_sphere: SphereMeshObstacle):
+        self.sphere_obstacles.append(new_sphere)
+        self.meshes.append(new_sphere.mesh)
+        self.joined_scene = join_meshes_as_scene(meshes=self.meshes, include_textures=False)
+    
+    def remove_sphere(self, sphere_idx: int):
+        self.sphere_obstacles.pop(sphere_idx)
+        self.meshes.pop(sphere_idx)
+        self.joined_scene = join_meshes_as_scene(meshes=self.meshes, include_textures=False)
+    
+    
+    def set_device(self, new_device: torch.device):
+        self.device = new_device
+        for sphere in self.sphere_obstacles:
+            sphere.set_device(new_device)
+
+
+class DepthMapRenderer:
+    def __init__(self, 
+                 device: torch.device, 
+                 scene: SphereScene, 
+                 camera: FoVPerspectiveCameras, 
+                 raster_settings: RasterizationSettings,
+                 MAX_MEASURABLE_DEPTH: float = 10.0,
+                 img_size: tuple = (240, 320)):
+        self.device = device
+        self.scene = scene
+        self.camera = camera
+        self.raster_settings = raster_settings
+        self.max_measurable_depth = MAX_MEASURABLE_DEPTH
+        self.rasterizer = MeshRasterizer(
+            cameras=camera, 
+            raster_settings=raster_settings
+        )
+        # k is a scaling factor for the distortion correction and is a function of FOV, sensor size, and focal length, etc.
+        # Initilized to 62.5 for the default FoVPerspectiveCamera settings with a 60 degree FOV and image size of 240x320
+        self.k = 62.5
+        self.img_size = img_size
+    
+    def render_depth_map(self):
+        """
+        Renders a depth map of the scene seen from the current camera position and orientation by performing rasterization 
+        and then correcting for perspective distortions. Also performs saturation of infinite and NaN depth values.
+        """
+        # Render depth map via rasterization
+        fragments = self.rasterizer(self.scene.joined_scene)
+        zbuf = fragments.zbuf
+        depth = torch.squeeze(zbuf).to(self.device)
+
+        # Saturate infinite and NaN depth values
+        depth[depth == -1.0] = self.max_measurable_depth
+        depth[depth >= self.max_measurable_depth] = self.max_measurable_depth
+
+        # Correct for perspective distortion
+        depth = self.correct_distorion(depth)
+        return depth
+
+    def correct_distorion(self, depth: torch.Tensor):
+        """
+        Corrects for perspective distortion in the depth map by calculating the depth values to the camera center
+        instead of to the image plane for each pixel in the image.
+        """
+
+        # Create grids of x and y coordinates
+        x_grid, y_grid = torch.meshgrid(torch.arange(self.img_size[0], device=device), torch.arange(self.img_size[1], device=self.device), indexing='ij')
+
+        # Compute distance from center for each pixel
+        center = torch.tensor(self.img_size, device=self.device)/2
+        dist_from_center = torch.norm(torch.stack([x_grid, y_grid], dim=-1) - center, dim=-1)
+
+        # Amplify dist tensor by 1/k
+        dist_from_center = dist_from_center / self.k
+
+        # Correct for perspective distortion at indices where depth is not infinite
+        depth = torch.where(depth < self.max_measurable_depth, torch.sqrt(torch.pow(depth,2) + torch.pow(dist_from_center,2)), depth).to(self.device)
+        depth[depth >= self.max_measurable_depth] = self.max_measurable_depth
+
+        return depth
+
+    def set_device(self, new_device: torch.device):
+        self.device = new_device
+        self.scene.set_device(new_device)
+        self.camera.to(new_device)
+    
+    def update_T(self, new_T: torch.Tensor):
+        new_T = new_T.to(self.device)
+        if new_T.shape != (1, 3):
+            raise ValueError("T must be a 1x3 tensor")
+        self.camera.T = new_T
+    
+    def update_R(self, new_R: torch.Tensor):
+        if new_R.shape == (3, 3):
+            new_R = new_R.unsqueeze(0).to(device)
+        self.camera.R = new_R
+
+    def save_depth_map(self, path: str, depth: torch.Tensor = None):
+        if depth is None:
+            depth = self.render_depth_map()
+        
+        plt.style.use('ggplot')
+        plt.rc('font', family='serif')
+        plt.rc('xtick', labelsize=12)
+        plt.rc('ytick', labelsize=12)
+        plt.rc('axes', labelsize=12)
+
+        plt.figure(figsize=(8, 6))
+        plt.imshow(depth.cpu().numpy(), cmap="magma")
+        plt.colorbar(label="Depth [m]", aspect=30, orientation="vertical", fraction=0.0235, pad=0.04)
+        plt.axis("off")
+        plt.savefig(path, bbox_inches='tight')
+
+unit_sphere_path = "./sphere.obj"
+
+pos1 = torch.tensor([0.0, 0.0, 0.0])
+pos2 = torch.tensor([0.0, 1.0, 0.0])
+pos3 = torch.tensor([0.0, -2.0, 0.0])
+pos4 = torch.tensor([2.0, 0.0, 0.0])
+pos5 = torch.tensor([2.0, 2.0, 0.0])
+pos6 = torch.tensor([-2.0, -2.0, 0.0])
+pos7 = torch.tensor([-2.0, 0.0, 0.0])
+
+sphere1 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=1.0, center_position=pos1)
+sphere2 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=1.0, center_position=pos2)
+sphere3 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=1.0, center_position=pos3)
+sphere4 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=1.0, center_position=pos4)
+sphere5 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=2.0, center_position=pos5)
+sphere6 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=0.8, center_position=pos6)
+sphere7 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=0.5, center_position=pos7)
+
+spheres = [sphere1, sphere2, sphere3, sphere4, sphere5, sphere6, sphere7]
+spherescene = SphereScene(device=device, sphere_obstacles=spheres)
+meshes = spherescene.joined_scene
+
+
+R = torch.tensor([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]).unsqueeze(0).to(device)
+T = torch.zeros((1, 3)).to(device)
+T[0, 0] = 0.0 # Move camera x units in the +X direction (inwards)
+T[0, 1] = 0.0 # Move camera x units in the +Y direction (upwards)
+T[0, 2] = 5.0 # Move camera x units in the +Z direction (in/out)
+#T = -T
+
+camera = FoVPerspectiveCameras(device=device, R=R, T=T, fov=FOV)
+#zfar_screen = camera.transform_points_screen(torch.tensor([[0.0, 0.0, MAX_MEASURABLE_DEPTH]]).to(device))[0, 2].item()
+#print(f"zfar_screen: {zfar_screen}")
+#camera = FoVPerspectiveCameras(device=device, R=R, T=T, fov=FOV, zfar=zfar_ndc)
+
+
+# Initialize rasterizer
+raster_settings = RasterizationSettings(
+    image_size=IMG_SIZE, 
+    blur_radius=0.0, 
+    faces_per_pixel=1, # Keep at 1, dont change
+    perspective_correct=True, # Doesn't do anything(??), but seems to improve speed
+    cull_backfaces=True # Do not render backfaces. MAKE SURE THIS IS OK WITH THE GIVEN MESH.
+)
+
+depth_renderer = DepthMapRenderer(device=device, 
+                            scene=spherescene, 
+                            camera=camera, 
+                            raster_settings=raster_settings, 
+                            MAX_MEASURABLE_DEPTH=MAX_MEASURABLE_DEPTH,
+                            img_size=IMG_SIZE)
+
+
+depth = depth_renderer.render_depth_map()
+depth_renderer.save_depth_map("./depth_map.pdf", depth)
+
+
+print(f"R:\n {R}")
+print(f"\nT:\n {T}")
+print(f'\nCamera position in world frame:\n {camera.get_camera_center()}')
+print(f"\nMin depth: {depth.min()}")
+print(f"\nMax depth: {depth.max()}")
+
+# #Time the rendering process fort 100 renders
+# import time
+# start = time.time()
+# n = 10000
+# for i in range(n):
+#     depth = depth_renderer.render_depth_map()
+# end = time.time()
+# print(f"Time taken for {n} renders: {end-start} seconds")
+# print(f"FPS: {n/(end-start)} seconds")
