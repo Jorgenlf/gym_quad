44d666ed319ecdef513e4a22c4092e121c8d86d7
Jorgen <jflind99@gmail.com>
2024-04-17 11:28:48 +0200

    Working rot trans of cam dependent on quad pos orient

--- gym_quad/objects/depth_camera.py
+++ gym_quad/objects/depth_camera.py
@@ -20,6 +20,21 @@ from pytorch3d.renderer import (
     TexturesVertex,
 )
 
+
+import sys
+import os
+#Use os and sys to access the modules inside gym_quad (imports below are from gym_quad)
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+# Get the parent and grandparent directory of the current script
+parent_dir = os.path.dirname(script_dir)
+grand_parent_dir = os.path.dirname(parent_dir)
+# Add the parent directory to the Python path
+sys.path.append(grand_parent_dir)
+
+import gym_quad.utils.geomutils as geom
+
+
 # Helper functions to transform between ENU and pytorch3D coordinate systems
 def enu_to_pytorch3d(enu_position: torch.Tensor) -> torch.Tensor:
     '''ENU is x-east, y-north, z-up. 
@@ -121,12 +136,16 @@ class SphereScene:
 pos1 = torch.tensor([0.0, 0.0, 0.0])
 pos2 = torch.tensor([0.0, 3.0, 0.0])
 pos3 = torch.tensor([0.0, 0.0, 1.5])
+pos4 = torch.tensor([1.0, 0.0, 1.5])
+pos5 = torch.tensor([2.0, -1.0, 0])
 unit_sphere_path = "./sphere.obj"
 sphere1 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=2.0, center_position=pos1)
 sphere2 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=0.5, center_position=pos2)
 sphere3 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=1.0, center_position=pos3)
+sphere4 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=1.2, center_position=pos4)
+sphere5 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=1.4, center_position=pos5)
 
-spheres = [sphere1,sphere2,sphere3]
+spheres = [sphere1,sphere2,sphere3,sphere4,sphere5]
 spherescene = SphereScene(device=device, sphere_obstacles=spheres)
 meshes = spherescene.scene
 
@@ -144,7 +163,7 @@ R = torch.tensor([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]).unsqueeze(
 T = torch.zeros((1, 3)).to(device)
 T[0, 0] = 0.0
 T[0, 1] = 0.0
-T[0, 2] = 6.0
+T[0, 2] = 0.0
 print(f"R: {R} with shape {R.shape}")
 print(f"T: {T} with shape {T.shape}")
 
@@ -165,35 +184,62 @@ rasterizer = MeshRasterizer(
 )
 
 #Generating n_steps positions and orientations for the camera in ENU frame
-n_steps = 8
+n_steps = 24
 positions = np.zeros((n_steps, 3)) # x, y, z in meters
 orientations = np.zeros((n_steps, 3)) # roll about x, pitch about y, yaw about z (ENU) in radians
+
+referencetype = 'line' # 'circle', 'line', 
+
 for i in range (n_steps):
     param = n_steps/2
-    positions[i] = np.array([6*np.cos(i/param*np.pi), 6*np.sin(i/param*np.pi), 0]) # x, y, z in meters 
-    orientations[i] = np.array([0, 0, i/param*np.pi]) # roll, pitch, yaw in radians
+    #Circle around origin with radius 6
+    if referencetype == 'circle':
+        positions[i] = np.array([6*np.cos(i/param*np.pi), 6*np.sin(i/param*np.pi), 0]) # x, y, z in meters 
+        orientations[i] = np.array([0, 0, np.pi + i/param*np.pi]) # roll, pitch, yaw in radians
+    elif referencetype == 'line':
+        positions[i] = np.array([i, 0, 0])
+        orientations[i] = np.array([0, 0, 0])
+
+if referencetype == 'circle': #Just use stuff made above
+    meshes = spherescene.scene
+elif referencetype == 'line':
+        obs1 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=2.0, center_position=torch.tensor([4, 0, 8]))
+        obs2 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=4.0, center_position=torch.tensor([2, 4, 5]))
+        obs3 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=2.3, center_position=torch.tensor([-4, 0, 12]))
+        obs4 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=2.1, center_position=torch.tensor([3, 0, 15]))
+        obs5 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=3.2, center_position=torch.tensor([0, -4, 17]))
+        obs6 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=2.3, center_position=torch.tensor([-2.7, 0, 18]))
+        obs7 = SphereMeshObstacle(device=device, path=unit_sphere_path, radius=1.0, center_position=torch.tensor([-0, 0, 26]))
+
+        spherescene = SphereScene(device=device, sphere_obstacles=[obs1, obs2, obs3, obs4, obs5, obs6, obs7])
+        meshes = spherescene.scene
 
 actual_camera_positions_enu = np.zeros((n_steps, 3))
 for i in range(n_steps):
-    T_enu = np_to_torch(positions[i])
-    Tstep = enu_to_pytorch3d(T_enu)
-    Tstep = Tstep.unsqueeze(0)
+    position = positions[i]
+    orientation = orientations[i]
+    
+    # Convert position and orientation to torch tensors
+    at = position + geom.Rzyx(*orientation) @ np.array([1, 0, 0])  # Look at the point in front of the camera along body x-axis
+    
+    at_torch = torch.from_numpy(at).to(device)
+    position_torch = torch.from_numpy(position).to(device)
 
-    #Create R matrix from ENU euler angles in radians
-    orientation_enu = np_to_torch(orientations[i])
-    orientation_pt3d = enu_to_pytorch3d(orientation_enu) #want roll,pitch,yaw ->  pitch,yaw,roll
-    Rstep = euler_angles_to_matrix(orientation_pt3d, convention="XYZ").unsqueeze(0) #TODO can probably use one convention which allows direct passage of enu angles ZXY maybe?
-
+    at_pt3d = enu_to_pytorch3d(at).to(device).float()
+    position_pt3d = enu_to_pytorch3d(position_torch).to(device).float()
+    # orientation_torch = torch.from_numpy(orientation).to(device)
+    
+    # Calculate rotation matrix
+    Rstep = look_at_rotation(position_pt3d[None, :], device=device, at=at_pt3d[None, :])  # (1, 3, 3)
+    # Calculate translation vector
+    Tstep = -torch.bmm(Rstep.transpose(1, 2), position_pt3d[None, :, None])[:, :, 0]   # (1, 3)
+    
     camera.T = Tstep.to(device)
     camera.R = Rstep.to(device)
 
     cam_pos = camera.get_camera_center()
-    print(f"Camera position: {cam_pos} with shape {cam_pos.shape}")
     actual_camera_positions_enu[i] = torch_to_np(pytorch3d_to_enu(cam_pos.reshape(3)))
     cam_orientation = camera.get_world_to_view_transform().get_matrix()    
-
-    # print(f"\nRstep: {Rstep} with shape {Rstep.shape}")
-    # print(f"Camera orientation: {cam_orientation}\n")
 
     #Render depth map
     rasterizer.cameras = camera
@@ -207,7 +253,6 @@ for i in range(n_steps):
 
     plt.figure()
     plt.imshow(depth.cpu().numpy(), cmap="magma")
-    plt.colorbar()
     plt.axis("off")
     plt.savefig(f"./test_img/depth_maps/depth_map{i}.png")
     plt.close()
---
+++ png2gif.py
@@ -0,0 +1,23 @@
+import imageio.v2 as imageio
+
+
+# Define filetype of source images:
+extension = '.png'
+
+# Option A: N filenames have common structure and are numbered in the correct sequence:
+#	    "image_name_0.png", "image_name_1.png", "image_name_2.png", ...
+# 	    n_imgs: known number of images
+n_imgs = 24
+path = "C:/Users/admin/Desktop/EirikJorgenMasterCode/gym_quad/gym_quad/objects/test_img/depth_maps/"
+filenames = [f"{path}depth_map{i}{extension}" for i in range(n_imgs)]
+
+# Option B: unknown filenames, but they are still ordered in ascending order by name:
+# import glob
+
+# filenames = sorted(glob.glob('*{extension}'))
+
+# Read, compose and write images to .gif:
+with imageio.get_writer('my_image_animation.gif', mode='I', duration=0.01, loop=0) as writer:
+    for filename in filenames:
+        image = imageio.imread(filename)
+        writer.append_data(image)
