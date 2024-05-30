624cc5ca523b3e969e50d0d0904256eec6ea07c9
Jorgen <jflind99@gmail.com>
2024-05-30 12:28:38 +0200

    can change fps now

--- png2gif.py
+++ png2gif.py
@@ -7,28 +7,32 @@ from PIL import Image
 # Define filetype of source images:
 extension = '.png'
 
-# Option A: N filenames have common structure and are numbered in the correct sequence:
+# Option known_n_imgs: N filenames have common structure and are numbered in the correct sequence:
 #	    "image_name_0.png", "image_name_1.png", "image_name_2.png", ...
 # 	    n_imgs: known number of images
 
-# Option B: unknown filenames, but they are still ordered in ascending order by name:
+# Option unknown_ordered: unknown filenames, but they are still ordered in ascending order by name:
 
-#Choose option A or B:
+#Choose option known_n_imgs or unknown_ordered:
 ###---###---###---### 
-option = 'B'
+option = 'unknown_ordered' # 'known_n_imgs' or 'unknown_ordered
 ###---###---###---###
 
-if option == 'A':
+###---### Define if you want a gif or a webp animation: ###---###
+mode = "webp" 
+###---###---###---###
+
+if option == 'known_n_imgs':
     n_imgs = 24
     path = "gym_quad/tests/test_img/depth_maps/"
     filenames = [f"{path}depth_map{i}{extension}" for i in range(n_imgs)]
     
-elif option == 'B':
+elif option == 'unknown_ordered':
     #Choose which depth maps to use:    
     ###---###---###---###
-    exp_id = 28
-    test_id = 5
-    scenario = "house"
+    exp_id = 32
+    test_id = 4
+    scenario = "house_easy_obstacles"
     ###---###---###---###
 
     # Regular expression to extract numbers from filenames:
@@ -39,11 +43,8 @@ elif option == 'B':
     path_pattern = f'log/LV_VAE_MESH-v0/Experiment {exp_id}/{scenario}/tests/test{test_id}/depth_maps/depth_map_*.png'
     filenames = sorted(glob.glob(path_pattern), key=sort_key)
 
-###---###---###---### Define if you want a gif or a webp animation:
-create_gif = False #IF false creates a webp animation instead (better quality)
-###---###---###---###
 
-if create_gif:
+if mode == "gif":
     print("There are ", len(filenames), " images in the folder\nBeginning to create gif... ",end="")
     
     #Frame duration (max 50 for gif):
@@ -56,18 +57,18 @@ if create_gif:
             writer.append_data(image)
     print("GIF created successfully!")
 
-elif not create_gif:
+elif mode == "webp":
     print("There are ", len(filenames), " images in the folder\nBeginning to create webp animation... ",end="")
 
     # Frame duration set for 90 FPS:
-    frame_duration = 1 / 100  # seconds per frame #TODO changing duration seemingly does nothing...
+    # frame_duration = 1 / 100  # seconds per frame #TODO changing duration seemingly does nothing...
 
     #IMAGEIOv2
     # Read, compose and write images to .webp:
-    with imageio.get_writer('my_image_animation.webp', mode='I', duration=frame_duration, loop=0, quality=100) as writer:
-        for filename in filenames:
-            image = imageio.imread(filename)
-            writer.append_data(image)
+    # with imageio.get_writer('my_image_animation.webp', mode='I', duration=frame_duration, loop=0, quality=100) as writer:
+    #     for filename in filenames:
+    #         image = imageio.imread(filename)
+    #         writer.append_data(image)
     
     #IMAGEIOv3
     # # Load images
@@ -76,7 +77,13 @@ elif not create_gif:
     # duration_per_frame = 10
     # # Save as WebP animation
     # iio.imwrite('my_image_animation.webp', images, duration=duration_per_frame, loop=0)
-
+    format = ".webp" 
+    output_path = "animated" + format
+    iio.imwrite(output_path, [iio.imread(fp) for fp in filenames],
+                    fps=30,  # Frames per second
+                    loop=0,  # 0 = infinite loop, 1 = no loop
+                    quantizer="nq",
+                    )
 
     #PILLOW
     # images = [Image.open(filename) for filename in filenames]
